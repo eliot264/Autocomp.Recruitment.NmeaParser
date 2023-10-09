22938991c987c5e2f3f863152da804b008607d4b
eliot264 <wk49439@zut.edu.pl>
2023-10-09 11:09:28 +0200

    Add NmeaMessageContentField class

---
+++ Autocomp.Nmea.Common/NmeaMessageContentField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autocomp.Nmea.Common
+{
+    internal abstract class NmeaMessageContentField
+    {
+        public abstract NmeaMessageContentField Parse(string s);
+        public abstract override string ToString();
+    }
+}
