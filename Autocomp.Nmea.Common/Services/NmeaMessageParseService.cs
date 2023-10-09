bc21b17c311a19b44356eeabd9f9b788bf2fd942
eliot264 <wk49439@zut.edu.pl>
2023-10-09 16:11:45 +0200

    Implement parse service

--- Autocomp.Nmea.Common/Services/NmeaMessageParseService.cs
+++ Autocomp.Nmea.Common/Services/NmeaMessageParseService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autocomp.Nmea.Common.Contents;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +9,33 @@ namespace Autocomp.Nmea.Common.Services
 {
     public class NmeaMessageParseService : INmeaMessageParseService
     {
+        private Dictionary<string, Type> messageTypes;
+
+        public NmeaMessageParseService()
+        {
+            messageTypes = new Dictionary<string, Type>
+            {
+                { "GLL", typeof(GLL) },
+                { "MWV", typeof(MWV) }
+            };
+        }
+
         NmeaMessageContent INmeaMessageParseService.Parse(string message)
         {
             throw new NotImplementedException();
         }
-
         NmeaMessageContent INmeaMessageParseService.Parse(NmeaMessage message)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Type contentType = messageTypes.Single(m => m.Key == message.Header).Value;
+
+                return (NmeaMessageContent)Activator.CreateInstance(contentType, new object[] { message });
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
     }
 }
