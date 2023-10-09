b09ad7eb56223fc4a0f34d64359db527d4d7ebd8
eliot264 <wk49439@zut.edu.pl>
2023-10-09 16:04:05 +0200

    refactor project hierarchy

---
+++ Autocomp.Nmea.Common/INmeaMessageParseService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autocomp.Nmea.Common
+{
+    internal interface INmeaMessageParseService
+    {
+        NmeaMessageContent Parse(string message);
+        NmeaMessageContent Parse(NmeaMessage message);
+    }
+}
