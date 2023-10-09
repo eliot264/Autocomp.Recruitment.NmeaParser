dccbedb8b724edfc2ac88c532ba45854ec446114
eliot264 <wk49439@zut.edu.pl>
2023-10-09 13:33:37 +0200

    Add UTCTimeOnly class

---
+++ Autocomp.Nmea.Common/ContentFields/UTCTimeOnly.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autocomp.Nmea.Common.ContentFields
+{
+    internal class UTCTimeOnly : NmeaMessageContentField<NmeaTimeOnly>
+    {
+        public override NmeaTimeOnly Value { get; set; }
+
+        public UTCTimeOnly(string s) : base(s) { }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        protected override NmeaTimeOnly Parse(string s)
+        {
+            if (s.Length != 9 && s.Length != 6 )
+            {
+                throw new ArgumentException($"String {s} is invalid. String must be in hhmmss.ss format.");
+            }
+
+            try
+            {
+                int hour = int.Parse(s.Substring(0, 2));
+                int minute = int.Parse(s.Substring(2, 2));
+                double second = double.Parse(s.Substring(4, 4));
+
+                return new NmeaTimeOnly()
+                {
+                    Hour = hour,
+                    Minute = minute,
+                    Second = second
+                };
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ Autocomp.Nmea.Common/NmeaTimeOnly.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autocomp.Nmea.Common
+{
+    public class NmeaTimeOnly
+    {
+        private int hour;
+        private int minute;
+        private double second;
+        public int Hour
+        {
+            get { return hour; }
+            set
+            {
+                if (value >=0 && value < 24)
+                {
+                    hour = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"Tried to add {value}. Hour takes values from 0 to 23");
+                }
+            }
+        }
+        public int Minute
+        {
+            get { return minute; }
+            set
+            {
+                if (value >= 0 || value < 60)
+                {
+                    hour = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"Tried to add {value}. Minute takes values from 0 to 59");
+                }
+            }
+        }
+        public double Second
+        {
+            get { return second; }
+            set
+            {
+                if (value >= 0 && value < 60.0)
+                {
+                    second = value;
+                }
+                else if(value == 60 || value == 61 || value == 62 || value == 63)
+                {
+                    second = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"Tried to add {value}. Second takes values from 0 to 59");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            switch (Second)
+            {
+                case 60:
+                    return "Time stamp is not available";
+                case 61:
+                    return "Positioning system is in manual input mode";
+                case 62:
+                    return "Electronic Position Fixing System operates in estimated (dead reckoning) mode";
+                case 63:
+                    return "Positioning system is inoperative";
+                default:
+                    return $"{hour:00}:{minute:00}:{second:00.00}";
+            }
+        }
+    }
+}
