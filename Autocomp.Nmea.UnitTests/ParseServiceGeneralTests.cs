1175a0345e3b5960215af2645bb0b5f60172aeac
eliot264 <wk49439@zut.edu.pl>
2023-10-10 13:51:26 +0200

    Add parse service general tests

--- Autocomp.Nmea.UnitTests/ParseServiceGeneralTests.cs
+++ Autocomp.Nmea.UnitTests/ParseServiceGeneralTests.cs
@@ -22,5 +22,24 @@ namespace Autocomp.Nmea.UnitTests
 
             Assert.ThrowsException<ArgumentException>(parseAction);
         }
+        [TestMethod]
+        public void InvalidMessageFormat()
+        {
+            INmeaMessageParseService parseService = new NmeaMessageParseService();
+            string invalidPrefixMessage = "#HDD,3953.88008971N,10506.75318910,W,034138.00,A,D*7A";
+            string invalidSuffixMessage = "$HDD,3953.88008971N,10506.75318910,W,034138.00,A,D#7A";
+            string invalidSeparatorMessage = "$HDD'3953.88008971N'10506.75318910'W,034138.00'A'D*7A";
+            string invalidTerminatorMessage = "$HDD,3953.88008971N,10506.75318910,W,034138.00,A,D*7A\n\n\n";
+
+            Action invalidPrefixAction = () => parseService.Parse(invalidPrefixMessage);
+            Action invalidSuffixAction = () => parseService.Parse(invalidSuffixMessage);
+            Action invalidSeparatorAction = () => parseService.Parse(invalidSeparatorMessage);
+            Action invalidTerminatorAction = () => parseService.Parse(invalidTerminatorMessage);
+
+            Assert.ThrowsException<ArgumentException>(invalidPrefixAction);
+            Assert.ThrowsException<ArgumentException>(invalidSuffixAction);
+            Assert.ThrowsException<ArgumentException>(invalidSeparatorAction);
+            Assert.ThrowsException<ArgumentException>(invalidTerminatorAction);
+        }
     }
 }
